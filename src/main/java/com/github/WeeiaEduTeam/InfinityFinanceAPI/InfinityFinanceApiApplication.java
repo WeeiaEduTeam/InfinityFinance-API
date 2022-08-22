ba78f1278d017a29707353c02ca8c82c0563c624
Rozumek29 <k.wyrozumialski@a-soft.pl>
2022-08-22 19:07:50 +0200

    Initial commit

---
+++ src/main/java/com/github/WeeiaEduTeam/InfinityFinanceAPI/InfinityFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.WeeiaEduTeam.InfinityFinanceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityFinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/WeeiaEduTeam/InfinityFinanceAPI/InfinityFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.WeeiaEduTeam.InfinityFinanceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
