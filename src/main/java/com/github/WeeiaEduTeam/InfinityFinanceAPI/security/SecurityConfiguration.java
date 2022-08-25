8fc6c61ee2626317eda1d394aae1f07d7a451f60
pacion <patgut5@o2.pl>
2022-08-25 11:34:27 +0200

    Init swagger

---
+++ src/main/java/com/github/WeeiaEduTeam/InfinityFinanceAPI/security/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.github.WeeiaEduTeam.InfinityFinanceAPI.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            // other public endpoints
+            "/api/v1/**",
+            "**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .build();
+    }
+}
