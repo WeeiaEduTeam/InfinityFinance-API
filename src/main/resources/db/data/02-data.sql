b511d220a9684c7ad6ec34694870c5e1a427b959
pacion <patgut5@o2.pl>
2022-09-09 14:26:00 +0200

    First init Make changelog Basic sql to check if initialization was made properly

---
+++ src/main/resources/db/data/02-data.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset pacion:2
+INSERT into category (name, created, updated) VALUES ('test', null, null);
+INSERT INTO role (name) VALUES ('testRole');
+INSERT INTO app_user (username, email, password, first_name, second_name, created, updated) VALUES ('test', 'test@wp.pl', 'test', 'test', 'test', null, null);
+INSERT INTO user_roles (role_id, user_id) VALUES(1, 1);
+INSERT INTO transaction (transaction_type, value, quantity, title, description, category_id, appuser_id, created, updated) VALUES ('INCOME', 12, 14, 're', 're', 1, 1, null, null)
